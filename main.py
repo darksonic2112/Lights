50ab95d6a54233049c743c4bdec204f2074c96e3
darksonic2112 <83122333+darksonic2112@users.noreply.github.com>
2023-11-27 13:57:07 +0100

    (incomplete) Implemented block drawings based on list element

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ pygame.init()
 maze_name = "lights-small-1.csv"
 maze_dir = ("lights-puzzles\\" + maze_name)
 
+white = (255, 255, 255)
+dark_grey = (25, 25, 25)
+yellow = (255, 255, 0)
+
+block_position_x = 0
+block_position_y = 0
+block_size = 30
+font = pygame.font.Font(None, 36)
 # Small size field: 14x14 blocks
 # Mid-size field: 25x25 blocks
 # Blocks are 40x40 px
@@ -14,25 +22,26 @@ if maze_name[6] == "-":
 else:
     window_width = 750
     window_height = 750
-
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Lights")
 
-white = (255, 255, 255)
-dark_grey = (25, 25, 25)
-yellow = (255, 255, 0)
-font = pygame.font.Font(None, 36)
-blocks_size = 30
 
-df = pd.read_csv(maze_dir, header=None, sep=";")
-print(df[1][0])
-
-
-def draw_rect():
+def draw_block(self, x, y):
     pass
 
 
-rect = (0, 0, blocks_size, blocks_size)
+df = pd.read_csv(maze_dir, header=None, sep=";")
+for row_letter in range(len(df)):
+    for column_letter in range(len(df[row_letter])):
+        if df.isnull()[row_letter][column_letter]:
+            rect = (block_position_x, block_position_y, block_size, block_size)
+            pygame.draw.rect(window, white, rect)
+            pygame.draw.rect(window, dark_grey, rect, 2)
+            block_position_x += 40
+            block_position_y += 40
+        else:
+            print(df[row_letter][column_letter])
+
 
 is_running = True
 while is_running:
@@ -44,8 +53,6 @@ while is_running:
             pass
 
     window.fill(dark_grey)
-    pygame.draw.rect(window, white, rect)
-    pygame.draw.rect(window, dark_grey, rect, 2)
 
     pygame.display.flip()
 
