49c73743511c0aeb35cb2245dcf5af08c82d8467
DARKSONIC <kevin-lehmann99@web.de>
2023-11-28 21:38:10 +0100

    Implemented draw field and draw block methods, but functionality is not fully there

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ white = (255, 255, 255)
 dark_grey = (25, 25, 25)
 yellow = (255, 255, 0)
 
-block_position_x = 0
-block_position_y = 0
 block_size = 30
 font = pygame.font.Font(None, 36)
 # Small size field: 14x14 blocks
@@ -25,22 +23,29 @@ else:
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Lights")
 
+global block_position_x, block_position_y
+block_position_x = 0
+block_position_y = 0
 
-def draw_block(self, x, y):
-    pass
 
+def draw_block(block_position_x, block_position_y):
+    rect = (block_position_x, block_position_y, block_size, block_size)
+    pygame.draw.rect(window, white, rect)
+    pygame.draw.rect(window, dark_grey, rect, 2)
 
-df = pd.read_csv(maze_dir, header=None, sep=";")
-for row_letter in range(len(df)):
-    for column_letter in range(len(df[row_letter])):
-        if df.isnull()[row_letter][column_letter]:
-            rect = (block_position_x, block_position_y, block_size, block_size)
-            pygame.draw.rect(window, white, rect)
-            pygame.draw.rect(window, dark_grey, rect, 2)
-            block_position_x += 40
-            block_position_y += 40
-        else:
-            print(df[row_letter][column_letter])
+def draw_field():
+    global block_position_x, block_position_y
+    df = pd.read_csv(maze_dir, header=None, sep=";")
+    for row_letter in range(len(df)):
+        for column_letter in range(len(df[row_letter])):
+            if df.isnull()[row_letter][column_letter]:
+                draw_block(block_position_x, block_position_y)
+                block_position_x += 40
+                if block_position_x == window_width:
+                    block_position_x = 0
+                    block_position_y += 40
+            else:
+                print(df[row_letter][column_letter])
 
 
 is_running = True
@@ -52,7 +57,8 @@ while is_running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pass
 
-    window.fill(dark_grey)
+    #window.fill(dark_grey)
+    draw_field()
 
     pygame.display.flip()
 
