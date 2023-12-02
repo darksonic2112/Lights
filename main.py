2aa07839ebbbc1f2e17ea86ac53844ee5f34bc3f
darksonic2112 <83122333+darksonic2112@users.noreply.github.com>
2023-12-02 06:52:22 +0100

    Implemented maze_field (not finished)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 pygame.init()
 #  Enter "lights1" (1-5) or "lights-small-1" (1-5) for the different mazes
-maze_name = "lights-small-1.csv"
+maze_name = "lights1.csv"
 maze_dir = ("lights-puzzles\\" + maze_name)
 
 white = (255, 255, 255)
@@ -19,11 +19,15 @@ if maze_name[6] == "-":
 else:
     window_width = 750
     window_height = 750
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Lights")
 
 block_size = 30
 font = pygame.font.Font(None, 30)
+
+rows, cols = (window_width // block_size, window_height // block_size)
+maze_field = [["white" for i in range(cols)] for j in range(rows)]
+
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Lights")
 
 light_bulb = pygame.image.load("assets\\lightbulb.png")
 
@@ -33,7 +37,7 @@ block_position_y = 0
 
 global numbers
 numbers = []
-for index in range(4):
+for index in range(5):
     numbers.append(index)
 
 global lit_blocks
@@ -59,8 +63,11 @@ def draw_dark_grey_block(block_position_x, block_position_y, number):
     number_text_rect.center = number_rect.center
     pygame.draw.rect(window, dark_grey, number_rect)
     pygame.draw.rect(window, dark_grey, number_rect, 2)
+    row = block_position_x // block_size
+    column = block_position_y // block_size
     text_x = number_rect.centerx - number_text_rect.width / 2
     text_y = number_rect.centery - number_text_rect.height / 2
+    #maze_field[][] = "dark_grey"
     window.blit(number_surface, (text_x, text_y))
     pygame.display.flip()
 
@@ -91,6 +98,12 @@ def draw_field():
                 if block_position_x == window_width:
                     block_position_x = 0
                     block_position_y += 30
+            elif df[row_letter][column_letter] == "x":
+                draw_dark_grey_block(block_position_x, block_position_y, -1)
+                block_position_x += 30
+                if block_position_x == window_width:
+                    block_position_x = 0
+                    block_position_y += 30
             elif df.isnull()[row_letter][column_letter]:
                 draw_white_block(block_position_x, block_position_y)
                 block_position_x += 30
@@ -115,8 +128,8 @@ while is_running:
             if mouse_x and mouse_y in lit_blocks:
                 print("Test")
             draw_light(mouse_x - (mouse_x % 30), mouse_y - (mouse_y % 30))
-
     draw_field()
+    print(maze_field)
 
     pygame.display.flip()
 
